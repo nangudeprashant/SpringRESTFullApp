069e1eb764bf0bd53aaba8cfea256289e7c3f084
Administrator <Administrator@DELL>
2019-11-08 18:09:12 +0530

    Initial commit.

---
+++ SpringRESTFullApp/src/main/java/com/javaLive/controller/NormalController.java
@@ -0,0 +1,10 @@
+package com.javaLive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+@Controller
+public class NormalController {
+
+}
